43fe1b0ed9338540344bebf1ea121b302e43f62c
André Souza <andregsouzaweb@gmail.com>
2024-05-26 11:38:35 -0300

    Understanding status

--- Desktop/new-portfolio/portfolio/src/components/sidebar.tsx
+++ Desktop/new-portfolio/portfolio/src/components/sidebar.tsx
@@ -10,13 +10,13 @@ export function Sidebar() {
   return (
     <div className="hidden md:block fixed top-0 left-0 h-full w-64 bg-gray-200 overflow-y-auto">
       <div className="flex h-full max-h-screen flex-col gap-2">
-        <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
+        <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:pl-6">
           <Link href="/" className="flex items-center gap-2 font-semibold">
             <span className="">Andre Souza</span>
           </Link>
         </div>
         <div className="flex-1">
-          <nav className="grid items-start px-2 text-sm font-medium lg:px-4">
+          <nav className="grid items-start pl-2 text-sm font-medium lg:pl-4">
             <Link
               href="/"
               className={`flex items-center gap-3 rounded-lg px-3 py-2 transition-all hover:text-primary ${pathname === "/" ? "bg-muted text-primary" : "text-muted-foreground"
